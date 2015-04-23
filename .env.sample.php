d5f25efbea9d9842ac07b55421c9bafe421b716e
Rifki Yandhi <rifkiyandhi@gmail.com>
2015-04-23 16:38:40 +0800

    update gitignore, add env sample

---
+++ .env.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'DEBUG_MODE' => true,
+	'NEO4J_DB_HOST' => "127.0.0.1",
+	'NEO4J_DB_PORT' => "4926",
+	'NEO4J_DB_USERNAME' => "neo4j",
+	'NEO4J_DB_PASSWORD' => "password",
+	'MYSQL_HOST' => "localhost",
+	'MYSQL_DATABASE' => "fisher",
+	'MYSQL_USERNAME' => "username",
+	'MYSQL_PASSWORD' => "password",
+];
