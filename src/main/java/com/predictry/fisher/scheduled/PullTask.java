0fceaffa0d4c24d36de483309a8e62d9a3422843
Jocki <jocki.predictry@gmail.com>
2015-06-24 15:58:19 +0800

    Add environment variable configuration to disable pull task from Tapirus while keep serving aggregation result.

--- src/main/java/com/predictry/fisher/scheduled/PullTask.java
+++ src/main/java/com/predictry/fisher/scheduled/PullTask.java
@@ -14,11 +14,23 @@ public class PullTask {
 
 	private static final Logger log = LoggerFactory.getLogger(PullTask.class);
 	
+	public static final String ENV_FISHER_PULL = "FISHER_PULL"; 
+	
 	@Autowired
 	private PullService pullService;
 	
+	/**
+	 * Periodically pull records from Tapirus.  To disable this pulling task while
+	 * keeping the server running (so it can serve), set an environment variable
+	 * with name <code>FISHER_PULL</code> with value <code>false</code>.
+	 */
 	@Scheduled(fixedRate=60000)
 	public void pullLog() {
+		String flag = System.getenv(ENV_FISHER_PULL);
+		if ((flag != null) && flag.toLowerCase().equals("false")) {
+			log.info("Pull task disabled.  " + ENV_FISHER_PULL + " [" + flag + "]");
+			return;
+		}
 		PullTime pullTime = pullService.getDefaultPullTime();
 		log.info("Processing aggregation for " + pullTime);
 		pullService.processAggregation(pullTime);
