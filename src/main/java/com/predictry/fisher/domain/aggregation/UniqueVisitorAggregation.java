44c7c29381f3cab6487d6958f852489af303cfe7
Jocki <jocki.predictry@gmail.com>
2015-07-23 13:17:41 +0800

    Fixes calculation of unique visitors.

--- src/main/java/com/predictry/fisher/domain/aggregation/UniqueVisitorAggregation.java
+++ src/main/java/com/predictry/fisher/domain/aggregation/UniqueVisitorAggregation.java
@@ -1,26 +1,37 @@
 package com.predictry.fisher.domain.aggregation;
 
+import static com.predictry.fisher.domain.util.Helper.getData;
+import static com.predictry.fisher.domain.util.Helper.getDataName;
+import static com.predictry.fisher.domain.util.Helper.getType;
+import static com.predictry.fisher.domain.util.Helper.isRecommended;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
-import static com.predictry.fisher.domain.util.Helper.*;
 
 import com.predictry.fisher.domain.stat.Stat;
 import com.predictry.fisher.domain.stat.Value;
 
 public class UniqueVisitorAggregation implements Aggregation {
 
-	private Set<String> existingIds = new HashSet<>();
+	private Set<String> existingSessions = new HashSet<>();
+	private Set<String> existingRecommendedSessions = new HashSet<>();
 	
 	@Override
 	public void consume(Map<String, Object> mapJson, Stat stat) {
-		if (getType(mapJson).equals("User") && getData(mapJson).containsKey("id")) {
-			String id = (String) getData(mapJson).get("id");
-			if (!existingIds.contains(id)) {
-				stat.addUniqueVisitor(new Value(1.0, isRecommended(mapJson)?1.0:0.0, 0.0));
-				existingIds.add(id);
+		if (getType(mapJson).equals("Action") && getDataName(mapJson).equals("VIEW")) {
+			Map<String,Object> data = getData(mapJson);
+			String session = data.get("session").toString();
+			double qtyOverall = 0.0, qtyRecommended = 0.0;
+			if (!existingSessions.contains(session)) {
+				qtyOverall = 1.0;
+				existingSessions.add(session);
 			}
+			if (isRecommended(mapJson) && !existingRecommendedSessions.contains(session)) {
+				qtyRecommended = 1.0;
+				existingRecommendedSessions.add(session);
+			}
+			stat.addUniqueVisitor(new Value(qtyOverall, qtyRecommended, 0.0));
 		}		
 	}
 
